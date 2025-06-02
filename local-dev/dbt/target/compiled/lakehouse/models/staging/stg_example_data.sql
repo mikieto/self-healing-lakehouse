5c021511b7dd1a440a71609e347ea0542a92f4b0
Miki Eto <mikieto@gmail.com>
2025-06-03 06:19:05 +0900

    Remove dbt generated files (target/ and logs/ directories)

--- local-dev/dbt/target/compiled/lakehouse/models/staging/stg_example_data.sql
+++
@@ -1,8 +0,0 @@
--- This is a simple example model.
--- It just selects some static values.
--- In a real project, this would typically select from a source table.
-
-select
-    1 as id,
-    'example_value' as description,
-    current_timestamp as loaded_at
